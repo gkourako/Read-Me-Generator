7c50f74313b39d61f50f81c11b1b8915d91fc201
gkourako <georgekourakos4@gmail.com>
2020-08-24 19:35:12 -0400

    Added more questions

--- index.js
+++ index.js
@@ -6,14 +6,65 @@ const generateMarkdown = require("./generateMarkdown")
 // array of questions for user
 const questions = [
 
+  {
+    // Title
+    type: "input",
+    message: "What is the title of your application?",
+    name: "title",
+  },
+
+  {
+    // Description
+    type: "input",
+    message: "Give a brief and to the point descripton of your application",
+    name: "description",
+
+  },
+  {
+    // Installation
+    type: "input",
+    message: "How does the user install your application? Be clear and consice with your instructions",
+    name: "installation",
+  },
+  {
+    // Usage
+    type: "input",
+    message: "How does the user use your application to its fullest extent?",
+    name: "",
+  },
+  {
+    // License
+    type: "input",
+    message: "What kind of licensing does your application have?",
+    name: "license",
+
+  },
+  {
+    // Contributing
+    type: "input",
+    message: "Are there any contributers to your application",
+    name: "license",
+
+  },
+  
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  // fs.writeFile()
 }
 
 // function to initialize program
 function init() {
+  inquirer.prompt(questions).then(answers => {
+    console.log(JSON.stringify(answers, null, 2));
+
+    //  Passes answers
+    const md = generateMarkdown(answers);
+    console.log(md)
+    //  pass result
+    //  writeToFile("README.md", md):
+  });
 
 }
 

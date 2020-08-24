a3c13fc0b21f2c9ff6be03a07a3df9bec468666c
gkourako <georgekourakos4@gmail.com>
2020-08-24 19:08:03 -0400

    finished mark down

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+
+${data.description}
+ ## Table of Contents
+
+ 1.[Installation] (#installation)
+ 2.[Usage] (#usage)
+ 3.[License] (#license)
+ 4.[Contributing] (#contributing)
+ 5.[Tests] (#tests)
+ 6.[Questions] (#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+You made it this far! If you have questions contact me on [GitHub] (http://github.com/${data.githubUsername}).  If not you can email me at ${data.email}
 
 `;
 }
